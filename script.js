eb6d629feb6e9f1623f89420ea9f870a9d8f8f44
elle <elle@.ru>
2021-12-19 21:24:56 +0300

    last

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var a = +prompt('Введите первое число');
 var b = +prompt('Введите второе число');
 var c = +prompt('Bведите третье число');
 
-if((a < b) && (a > c)){
-    alert(a);
-}else if((a > b) && (b > c)){
-    alert(b);
-}else if((c < a) && (c > b)){
-    alert(c);
-}else{
+if(a < b && a > c && !isNaN(a) && !isNaN(b) && !isNaN(c)){
+    alert('Среднее число ' + a);
+}else if(a > b && b > c && !isNaN(a) && !isNaN(b) && !isNaN(c)){
+    alert('Среднее число ' + b);
+}else if(c < a && c > b && !isNaN(a) && !isNaN(b) && !isNaN(c)){
+    alert('Среднее число ' + c);
+}else {
     alert('Математика')
 }
